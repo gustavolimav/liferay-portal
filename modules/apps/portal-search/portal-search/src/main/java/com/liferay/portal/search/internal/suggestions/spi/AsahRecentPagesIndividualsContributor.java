266fda673db43390ffbd2f37dd07bccfaebc25cc
gustavolimav <gustavo.lima@liferay.com>
2023-10-30 17:34:17 -0300

    LPS-174758 LPS-200418 portal-search: Implement new Contributors

---
+++ modules/apps/portal-search/portal-search/src/main/java/com/liferay/portal/search/internal/suggestions/spi/AsahRecentPagesIndividualsContributor.java
@@ -0,0 +1,58 @@
+/**
+ * SPDX-FileCopyrightText: (c) 2000 Liferay, Inc. https://liferay.com
+ * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
+ */
+
+package com.liferay.portal.search.internal.suggestions.spi;
+
+import com.liferay.analytics.settings.rest.manager.AnalyticsSettingsManager;
+import com.liferay.portal.kernel.portlet.LiferayPortletRequest;
+import com.liferay.portal.kernel.portlet.LiferayPortletResponse;
+import com.liferay.portal.kernel.search.SearchContext;
+import com.liferay.portal.search.rest.dto.v1_0.SuggestionsContributorConfiguration;
+import com.liferay.portal.search.spi.suggestions.SuggestionsContributor;
+import com.liferay.portal.search.suggestions.SuggestionBuilderFactory;
+import com.liferay.portal.search.suggestions.SuggestionsContributorResults;
+import com.liferay.portal.search.suggestions.SuggestionsContributorResultsBuilderFactory;
+
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+
+/**
+ * @author Gustavo Lima
+ */
+@Component(
+	configurationPid = "com.liferay.portal.search.internal.configuration.AsahIndividualsConfiguration",
+	property = "search.suggestions.contributor.name=asahRecentPages",
+	service = SuggestionsContributor.class
+)
+public class AsahRecentPagesIndividualsContributor
+	extends BaseAsahKeywordsSuggestionsContributor
+	implements SuggestionsContributor {
+
+	@Override
+	public SuggestionsContributorResults getSuggestionsContributorResults(
+		LiferayPortletRequest liferayPortletRequest,
+		LiferayPortletResponse liferayPortletResponse,
+		SearchContext searchContext,
+		SuggestionsContributorConfiguration
+			suggestionsContributorConfiguration) {
+
+		return getSuggestionsContributorResults(
+			_analyticsSettingsManager, searchContext,
+			"rangeKey,displayLanguageId,page,size,sort",
+			_suggestionBuilderFactory, suggestionsContributorConfiguration,
+			_suggestionsContributorResultsBuilderFactory);
+	}
+
+	@Reference
+	private AnalyticsSettingsManager _analyticsSettingsManager;
+
+	@Reference
+	private SuggestionBuilderFactory _suggestionBuilderFactory;
+
+	@Reference
+	private SuggestionsContributorResultsBuilderFactory
+		_suggestionsContributorResultsBuilderFactory;
+
+}
